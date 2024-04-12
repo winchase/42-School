7a3adee681a9743bc1553462ba1f12d3afab04a1
winchase <serggorely8@gmail.com>
2024-04-12 02:32:11 +0200

    added ft_print_alphabet

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+char alpha = 'a';
+
+	while(alpha <= 'z')
+	{
+	write(1 ,&alpha, 1);
+	alpha++;
+	}
+}
+
+int main(void)
+{
+ft_print_alphabet();
+return 0;
+}
