bbc1133bd61f2736aa8b349468007aa2690f4005
winchase <serggorely8@gmail.com>
2024-04-17 01:22:48 +0200

    added ex01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,25 @@
+void    ft_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
+
+//int main(void)
+//{
+//
+//    int a = 0;
+//    int *ptr1 = &a;
+//    int **ptr2 = &ptr1;
+//    int ***ptr3 = &ptr2;
+//    int ****ptr4 = &ptr3;
+//    int *****ptr5 = &ptr4;
+//    int ******ptr6 = &ptr5;
+//    int *******ptr7 = &ptr6;
+//    int ********ptr8 = &ptr7;
+//    int *********ptr9 = &ptr8;
+//
+//    printf("Before: %d\n",a);
+//    ft_ft(ptr9);
+//    printf("After: %d\n",a);
+//
+//    return 0;
+//}
