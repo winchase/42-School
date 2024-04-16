6656774235c468994e26e5e444d432b90309fe33
winchase <serggorely8@gmail.com>
2024-04-17 01:22:35 +0200

    added ex00

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void	ft_ft(int *nbr)
+{
+	*nbr=42;
+}
+//int main(void)
+//{
+
+//    int a = 0;
+
+  //  printf("Before: %d\n",a);
+    //ft_ft(&a);
+    //printf("After: %d\n",a);
+//  return 0;
+//}
